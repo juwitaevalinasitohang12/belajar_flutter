6fc65a67ec64914a4cbbe7ae4bdcc15820d02de5
Juwitaevalinasitohang12 <juwitaevalinasitohang12@gmail.com>
2026-08-25 16:50:18 +0800

    feat: mengubah teks penghitung

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Jumlah tombol yang Ditekan:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
